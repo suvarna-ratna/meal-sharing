f80b2fb45373793212a0f505a5b5f37b7532deb2
suvarna <suvarna.akkala@gmail.com>
2021-12-20 19:21:44 +0100

    pics on aboutus

--- src/client/components/AboutUs/AboutUs.js
+++ src/client/components/AboutUs/AboutUs.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./AboutUs.css";
-import { download } from "../../assets/images/download.png";
+import foodshare from "../../assets/images/foodshare.png";
 
 const Aboutus = () => {
   return (
     <div className="about">
       <h4>Food sharing</h4>
-      <img src={download} alt="food Sharing" />
+      <img src={foodshare} alt="food Sharing"/>
       <h5>
         Whether it’s fruit, vegetables, or dairy, there’s a lot of food waste in
         the world each day. Luckily, many new food sharing apps are working to
