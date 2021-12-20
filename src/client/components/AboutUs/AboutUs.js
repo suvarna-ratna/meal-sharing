e512eb166f3a189996ea108f3b94af8f1c163443
suvarna <suvarna.akkala@gmail.com>
2021-12-20 13:02:06 +0100

    img added

--- src/client/components/AboutUs/AboutUs.js
+++ src/client/components/AboutUs/AboutUs.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React from "react";
 import "./AboutUs.css";
+import pic from "../../assets/images/foodshare.png";
 
 const Aboutus = () => {
-    return (
-        <div className='about'>
-            <h4>Food sharing remains a fantastic option to enjoy delicious cuisine while also reducing global food waste. Here is our Meal-Sharing apps motive.</h4>
-        <img src="download.jpeg" alt="food Sharing pic" />
-        <h5>Whether it’s fruit, vegetables, or dairy, there’s a lot of food waste in the world each day.
-          Luckily, many new food sharing apps are working to provide solutions!</h5>
-        <hr />
-        <p>The sharing culture has become an important part of how people provide and access services in recent years.
-          It helps to bridge the gap between those who have an abundance and those who have significant needs.
-          It's also a wonderful way to forge meaningful connections as we can share new experiences through food and family.
-
-          Whether companies are looking for ways to reduce food waste or families are looking for a homemade entrée, food sharing apps can help bring communities together.
-          Do you all have any favorite food sharing apps? Let us know on social #getMealSharingApp!</p>
-        </div>
-    );
-}
+  return (
+    <div className="about">
+      <h4>
+        Food sharing remains a fantastic option to enjoy delicious cuisine while
+        also reducing global food waste. Here is our Meal-Sharing apps motive.
+      </h4>
+      <img src={pic} alt="food Sharing pic" />
+      <h5>
+        Whether it’s fruit, vegetables, or dairy, there’s a lot of food waste in
+        the world each day. Luckily, many new food sharing apps are working to
+        provide solutions!
+      </h5>
+      <hr />
+      <p>
+        The sharing culture has become an important part of how people provide
+        and access services in recent years. It helps to bridge the gap between
+        those who have an abundance and those who have significant needs. It's
+        also a wonderful way to forge meaningful connections as we can share new
+        experiences through food and family. Whether companies are looking for
+        ways to reduce food waste or families are looking for a homemade entrée,
+        food sharing apps can help bring communities together. Do you all have
+        any favorite food sharing apps? Let us know on social
+        #getMealSharingApp!
+      </p>
+    </div>
+  );
+};
 
 export default Aboutus;
